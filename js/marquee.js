0b885cbfe621c4722e78acbbdaba970c88da7eff
J. Matthew Landis <jml168@pitt.edu>
2018-01-24 18:09:26 -0500

    Initial Commit

---
+++ js/marquee.js
@@ -0,0 +1,111 @@
+/* JavaScript Document */
+
+var marqueeVars = {
+  screenSize : '',
+  width : 0,
+  mobileSize : 600,
+  autoPlay : true,
+  currentPanel : 1,
+  totalPanels : 0,
+  timePassed : 0,
+  timeToChange : 60,
+  duration : 1250,
+  inTransition : false,
+  panelContent : Array
+};
+
+var headers = new Array();
+var taglines = new Array();
+
+
+$(document).ready(function(){
+  marqueeGatherData();
+});
+
+function marqueeGatherData(){
+  $('.marquee_data .marquee_panel').each(function(index) {
+    marqueeVars.totalPanels = (index + 1);
+    var panel_image_l = $(this).attr('data-image') + '_l.jpg';
+    var panel_image_s = $(this).attr('data-image') + '_s.jpg';
+    var panel_head = $(this).attr('head_title');
+    var tagline = $(this).attr('tag-line');
+    var panel_caption = $(this).html();
+    headers[index] = panel_head;
+    taglines[index] = tagline;
+    marqueeVars.panelContent[index] = '<div class="marquee_panel" data-image-s="'+panel_image_s+'" style="background-image:url('+panel_image_l+');"><div class="overlay"></div><div class="panel_caption">'+panel_caption+'</div></div>';
+  });
+  var marqueeTimer = setInterval(marqueeAdvance, 100);
+}
+
+function marqueeAdvance(){
+  var marqueeWidth = $('.marquee').width();
+  var currentSize = marqueeVars.screenSize;
+
+  if(marqueeWidth > marqueeVars.mobileSize) {
+    var newSize = 'large';
+  } else {
+    var newSize = 'small';
+  }
+  marqueeVars.screenSize = newSize;
+
+  if(currentSize != newSize){
+    if(marqueeVars.screenSize == 'large'){
+      marqueeMultiPanel();
+    }
+  }
+
+  if(marqueeVars.timePassed == marqueeVars.timeToChange){
+    marqueeVars.timePassed = 0;
+  } else {
+    marqueeVars.timePassed += 1;
+  }
+}
+
+function marqueeMultiPanel(){
+  marqueeVars.timePassed = 0;
+  marqueeVars.autoPlay = true;
+  var newHTML = '<div class="marquee_stage_large"><div class="marquee_container_1"></div><div class="btn prev"></div><div class="btn next"></div></div>';
+  $('.marquee').html('').append(newHTML);
+
+  var tab_height = Math.floor(620 / marqueeVars.totalPanels);
+  for(i=0; i<marqueeVars.totalPanels; i++) {
+      $('.marquee_nav').append('<div class="tabitem" style="height: '+tab_height+'px;"><div class="tabcontent"><h2>'+headers[i]+'</h2><p>'+taglines[i]+'</p></div></div>');
+  };
+
+  $('.marquee').hover(function(){
+    marqueeVars.autoPlay = false;
+  },function(){
+    marqueeVars.autoPlay = true;
+    marqueeVars.timePassed = Math.floor(marqueeVars.timeToChange / 2);
+  });
+
+  $('.marquee_nav div').on('click',function(){
+    var navClicked = $(this).index();
+    marqueeVars.currentPanel = navClicked + 1;
+
+
+    $('.marquee_stage_large').append('<div class="marquee_container_2" style="opacity:0;"></div>');
+    $('.marquee_container_2').html(marqueeVars.panelContent[navClicked]).animate({opacity:1},marqueeVars.duration,function(){
+      $('.marquee_container_1').remove();
+      $(this).addClass('marquee_container_1').removeClass('marquee_container_2');
+    });
+  });
+
+  $('.marquee_nav div:first').trigger('click');
+}
+
+
+// debugger
+var debugTimer = setInterval(setDebugger,100);
+function setDebugger() {
+  $('.var1').html('screenSize = '+marqueeVars.screenSize);
+  $('.var2').html('width = '+marqueeVars.width);
+  $('.var3').html('mobileSize = '+marqueeVars.mobileSize);
+  $('.var4').html('autoPlay = '+marqueeVars.autoPlay);
+  $('.var5').html('currentPanel = '+marqueeVars.currentPanel);
+  $('.var6').html('totalPanels = '+marqueeVars.totalPanels);
+  $('.var7').html('timePassed = '+marqueeVars.timePassed);
+  $('.var8').html('timeToChange = '+marqueeVars.timeToChange);
+  $('.var9').html('duration = '+marqueeVars.duration);
+
+}
